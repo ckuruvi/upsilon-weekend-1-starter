e6e59bc1f531a8b337ab79b35500f25e02923ad2
charles <charles@charless-MacBook-Pro.local>
2017-01-07 17:34:13 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Weekend 1 Challenge Starter Repo
 
-If you want, you can start your project by forking and cloning this repo.
+*Author: Charles Kuruvila*
+*Date : 01/07/2017*
 
-However, feel free to start from scratch,too.
-
-**REMEMBER:** Carefully read the instructions in the assignment app.
+1. Data entered  into the text fields are appended to the DOM below the form.
+2. Validation implemented onto the inputs field through *HTML pattern* attribute.
+3. Annual salary appended to Monthly Salary Expenditure set as *Read Only* field.
+4. User has the ability to delete one row at a time. *Monthly Salary Expenditure* is *updated* accordingly.
--- app.js
+++ app.js
@@ -1,33 +1,65 @@
-$(function () {
-  console.log('document is ready');
+  $(function () {
+    console.log('document is ready');
 
-  $('form').on('submit', function (event) {
-    console.log('form submitted');
+    $('form').on('submit', function (event) {
+      console.log('form submitted');
 
-    event.preventDefault();
+      event.preventDefault();
 
-    var formData = {};
-    var formAsArray = $(this).serializeArray();
+      var formData = {};
+      var formAsArray = $(this).serializeArray();
 
-    formAsArray.forEach(function (input) {
-      formData[input.name] = input.value;
+      formAsArray.forEach(function (input) {
+        formData[input.name] = input.value;
+      });
+
+
+      //appendDom(formData);
+      appendToTable(formData);
+      clearForm();
     });
 
-    appendDom(formData);
+    $('#employees').on('click','button',function(event){
+      console.log("*********inside delete button event**********");
+      console.log('click delete button'+this);
+      console.log('click delete button jquery value'+$(this));
+      var totalExp=$('#totalExpenditure').find('input[type=text]').val();
+      var currentEmpAnnualSal=$(this).closest('tr').find('#empSal').text();
+      $('#totalExpenditure').find('input[type=text]').val(Number(totalExp)-Number(currentEmpAnnualSal));
+      console.log("empSalary"+currentEmpAnnualSal);
+      $(this).closest('tr').remove();
 
-    clearForm();
+
+    });
   });
-});
 
-function appendDom(emp) {
-  var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+  function appendToTable(emp){
 
-  $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
-  $emp.append('<p>' + emp.employeeIdNumber + '</p>');
+    var $emp=$('<tr>');
+    $emp.append('<td>'+emp.employeeFirstName+'</td>');
+    $emp.append('<td>'+emp.employeeLastName+'</td>');
+    $emp.append('<td>'+emp.employeeIdNumber+'</td>');
+    $emp.append('<td>'+emp.employeeJobTitle+'</td>');
+    $emp.append('<td id="empSal">'+emp.employeeAnnualSalary+'</td>');
+    $emp.append('<button id="deleteButton">Delete Row</button>');
+    $emp.append('</tr>');
+    console.log($emp);
+    $('#employee').append($emp);
+    var salary=emp.employeeAnnualSalary;
+    var totalExp=$('#totalExpenditure').find('input[type=text]').val();
+    var newTotalExp=Number(salary)+Number(totalExp);
+    $('#totalExpenditure').find('input[type=text]').val(newTotalExp);
+  }
 
-  $('#employees').append($emp); // append our div to the DOM
-}
+  // function appendDom(emp) {
+  //   var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+  //
+  //   $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
+  //   $emp.append('<p>' + emp.employeeIdNumber + '</p>');
+  //
+  //   $('#employees').append($emp); // append our div to the DOM
+  // }
 
-function clearForm() {
-  $('form').find('input[type=text]').val('');
-}
+  function clearForm() {
+    $('form').find('input[type=text]').val('');
+  }
